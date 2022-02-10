d649d4e31cd76c43bd3d6e045b0e6d40568d67ba
Michielvdz <r0750793@student.thomasmore.be>
2022-02-10 23:30:34 +0100

    updates to deploy to heroku

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/project4web'));
+app.use(express.static(__dirname + ''));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/project4web/index.html'));});
+    '/index.html'));});
 app.listen(process.env.PORT || 8080);
 
 
